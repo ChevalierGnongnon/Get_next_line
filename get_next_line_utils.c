dccc9d6c2ba80efe940cce5408d8bfbc3915cc89
Chems Hoflack <chhoflac@k1r2p7.42mulhouse.fr>
2023-12-17 16:16:47 +0100

    ajout brainstorm

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,11 +6,12 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 13:52:38 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/12/17 15:07:02 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/12/17 15:15:51 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 size_t	ft_strlen(char *s)
 {
@@ -20,4 +21,11 @@ size_t	ft_strlen(char *s)
 	while (s[i])
 		i++;
 	return (i);
+}
+
+int main(void)
+{
+	char	*s = "abc";
+
+	printf("%ld", ft_strlen(s));
 }
